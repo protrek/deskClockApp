20a3618909157c4607f2162cbf67c252c385f1be
protrek <iprotrek@gmail.com>
2026-02-25 20:35:05 -0800

    Initial Commit

---
+++ deskClockApp/deskClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  deskClockAppApp.swift
+//  deskClockApp
+//
+//  Created by protrek on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct deskClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
